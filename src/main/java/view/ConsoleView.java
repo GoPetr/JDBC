c3ae400559a3e0294eb29d909a846e2f47d61d89
GoPetr <gopetr@gmail.com>
2022-06-06 13:48:36 +0300

    add view console logic

--- src/main/java/view/ConsoleView.java
+++ src/main/java/view/ConsoleView.java
@@ -1,9 +1,5 @@
 package view;
 
-import model.Developer;
-import model.Skill;
-import model.Specialty;
-
 import java.util.Scanner;
 
 public class ConsoleView {
@@ -30,119 +26,69 @@ public class ConsoleView {
 
 
       // Блок кода skill
-      if (line.equals("crs")) {
+      if (line.equals("crskill")) {
         skillView.createSkill();
       }
-      if (line.equals("upds")) {
+      if (line.equals("updskill")) {
         skillView.updateSkill();
       }
-      if (line.equals("ids")) {
+      if (line.equals("idskill")) {
         skillView.getById();
       }
-      if (line.equals("alls")) {
+      if (line.equals("allskill")) {
         skillView.getAllSkills();
       }
-      if (line.equals("dels")) {
+      if (line.equals("delskill")) {
         skillView.deleteSkill();
       }
 
       //Блок кода specialties
-      if (line.equals("cr")) {
+      if (line.equals("crspec")) {
         specialtyView.createSpecialty();
       }
-      if (line.equals("up")) {
+      if (line.equals("updspec")) {
         specialtyView.updateSpecialty();
       }
-      if (line.equals("i")) {
+      if (line.equals("idspec")) {
         specialtyView.getById();
       }
-      if (line.equals("a")) {
+      if (line.equals("allspec")) {
         specialtyView.getAllSpecialty();
       }
-      if (line.equals("d")) {
+      if (line.equals("delspec")) {
         specialtyView.deleteSpecialty();
       }
 
       //Блок кода developer
-      if (line.equals("dev")) {
+      if (line.equals("crdev")) {
         developerView.createDeveloper();
       }
-      if (line.equals("upd")) {
+      if (line.equals("upddev")) {
         developerView.updateDeveloper();
       }
-      if (line.equals("id")) {
+      if (line.equals("iddev")) {
         developerView.getById();
       }
-      if (line.equals("all")) {
+      if (line.equals("alldev")) {
         developerView.getAllDevelopers();
       }
-      if (line.equals("del")) {
+      if (line.equals("deldev")) {
         developerView.deleteDeveloper();
       }
     }
   }
 
-  public void create() {
-    Developer developer = new Developer();
-
-    Scanner scanner = new Scanner(System.in);
-
-    System.out.println("Create developer. Please enter some field");
-
-    System.out.print("Please enter firstname: ");
-    developer.setFirstName(scanner.nextLine());
-    System.out.println();
-
-    System.out.print("Please enter lastname: ");
-    developer.setLastName(scanner.nextLine());
-    System.out.println();
-
-    while (true) {
-      Skill skill = new Skill();
-      System.out.println("Please enter skills: ");
-      skill.setSkill(scanner.nextLine());
-      developer.setSkills(skill);
-      System.out.println();
-      System.out.println("Do you want to add skills?(y or n)");
-      String a = scanner.nextLine();
-      // как сделать что бы сравнение было scanner.equals("n") java позволяет это сделать. Но проблема,
-      // что один Object а второй String. Можно ли без записи String a = scanner.nextLine()?
-      if (a.equals("n")) {
-        break;
-      }
-    }
-
-    Specialty specialty = new Specialty();
-    System.out.println("Please enter specialty: ");
-    specialty.setSpecialty(scanner.nextLine());
-    developer.setSpecialty(specialty);
-    System.out.println();
-
-    //  repository.save(developer);
-    // repository.developerList.add(developer);
-    // GsonDeveloperRepositoryImpl.developerList.add(developer);
-  }
-
-  public void read() {
-    System.out.println("List of developers: ");
-//    List<Developer> allDevelopers = repository.getAllDevelopers();
-//    for (Developer o : allDevelopers) {
-//      System.out.println(o);
-  }
-//    for (Developer o : repository.getDeveloperList()) {
-//      System.out.println(o);
-//    }
-//    for (Developer o : GsonDeveloperRepositoryImpl.developerList) {
-//      System.out.println(o);
-//    }
-
-
   public void help() {
     System.out.println("Command list: -help");
-    System.out.println("quite");
-    System.out.println("create");
-    System.out.println("read");
-    System.out.println("update");
-    System.out.println("delete");
+    System.out.println("List command for Developer:");
+    System.out.print("crdev, upddev, iddev, alldev, deldev");
+    System.out.println();
+    System.out.println("List command for Skill:");
+    System.out.print("crskill, updskill, idskill, allskill, delskill");
+    System.out.println();
+    System.out.println("List command for Specialties:");
+    System.out.print("crspec, updspec, idspec, allspec, delspec");
+    System.out.println();
+
   }
 }
